277c6c8b7d733baae6249e4285f2f9e5c9b52b70
Timo Stollenwerk <tisto@plone.org>
2016-03-14 13:49:03 +0100

    Add new substring_match test.

---
+++ test_substring_match.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+from fixtures import *
+from pysolr import SolrCoreAdmin
+import shutil
+
+
+@pytest.fixture(scope="module", autouse=True)
+def solr(request, solr_core_name='substring_match'):
+    solr_core = setup_solr_core(solr_core_name)
+
+    def fin():
+        core_admin = SolrCoreAdmin('http://localhost:8989/solr/admin/cores')
+        core_admin.unload(solr_core_name)
+        target_dir = 'test-solr/solr/{}'.format(solr_core_name)
+        if os.path.isdir(target_dir):
+            shutil.rmtree(target_dir)
+
+    request.addfinalizer(fin)
+
+    return solr_core
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_solr(solr_base):
+    solr_base.delete(q='*:*')
+
+
+def test_substring_match_exact(solr):
+    solr.add([{
+        'id': '1',
+        'substring_match': 'Colorless Green Ideas Sleep Furiously',
+    }])
+
+    result = solr.search(
+        'substring_match:"Colorless Green Ideas Sleep Furiously"'
+    )
+
+    assert 1 == result.hits
+    assert u'Colorless Green Ideas Sleep Furiously' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_ignores_lowercase(solr):
+    index = 'Colorless Green Ideas Sleep Furiously'
+    query = 'colorless green ideas sleep furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'Colorless Green Ideas Sleep Furiously' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_ignores_uppercase(solr):
+    index = 'colorless green ideas sleep furiously'
+    query = 'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'colorless green ideas sleep furiously' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_ignores_punctuation(solr):
+    index = 'Colorless, Green; Ideas. Sleep? Furiously!'
+    query = 'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'Colorless, Green; Ideas. Sleep? Furiously!' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_trims_whitespace(solr):
+    index = '  Colorless Green Ideas Sleep Furiously         '
+    query = 'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'  Colorless Green Ideas Sleep Furiously         ' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_trims_inner_whitespace(solr):
+    index = 'Colorless    Green Ideas Sleep Furiously'
+    query = 'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'Colorless    Green Ideas Sleep Furiously' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_replaces_non_ascii_characters(solr):
+    index = u'Cölorless Grêen Idéaß Slèep Furiously'
+    query = u'Colorless Green Ideass Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert u'Cölorless Grêen Idéaß Slèep Furiously' == \
+        [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_ignores_special_characters(solr):
+    index = u'Colorless Green-Ideas Sleep=Furiously #?/[]()'
+    query = u'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert index == [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_ignores_content_in_brackets(solr):
+    index = u'Colorless Green Ideas Sleep Furiously (or not)'
+    query = u'Colorless Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert index == [x.get('substring_match') for x in result][0]
+
+
+def test_substring_match_replaces_ampersands_with_and(solr):
+    index = u'Colorless & Green Ideas Sleep Furiously'
+    query = u'Colorless and Green Ideas Sleep Furiously'
+    solr.add([{
+        'id': '1',
+        'substring_match': index,
+    }])
+
+    result = solr.search(
+        'substring_match:"{}"'.format(query)
+    )
+
+    assert 1 == result.hits
+    assert index == [x.get('substring_match') for x in result][0]
